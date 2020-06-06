3fa82680b1a59320b259861ae5a9a63624e731b1
Douglas Fleischut <df@douglass-imac.lan>
2020-06-06 07:59:22 -0700

    workshop changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselPause").click(function(){
-        $(".carousel").carousel("pause");
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
     });
-    $("#carouselPlay").click(function(){
-        $(".carousel").carousel("cycle");
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
     });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+
+
 });
