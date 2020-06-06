6c1d7268f7430f3e3df3666593dca14685f353d9
Douglas Fleischut <df@douglass-imac.lan>
2020-06-06 11:06:37 -0700

    erased js

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselPause").click(function(){
-        $(".carousel").carousel("pause");
-    });
-    $("#carouselPlay").click(function(){
-        $(".carousel").carousel("cycle");
-    });
-});
